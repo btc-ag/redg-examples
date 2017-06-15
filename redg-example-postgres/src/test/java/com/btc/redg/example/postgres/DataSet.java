bcbd4328e3dfa033559f2f42653a6023a34dbd18
Ole Wehrmeyer <Ole.Wehrmeyer@btc-ag.com>
2017-06-15 11:54:19 +0200

    Make examples work with snapshot version

--- redg-example-postgres/src/test/java/com/btc/redg/example/postgres/DataSet.java
+++ redg-example-postgres/src/test/java/com/btc/redg/example/postgres/DataSet.java
@@ -12,7 +12,6 @@ public class DataSet {
         RedG redG = new RedG();
         redG.setPreparedStatementParameterSetter(new MyPreparedStatementParameterSetter());
 
-        // NOTE: This currently fails due to a bug in the runtime. It will be fixed in version 1.0.18
         redG.addAppointment(redG.dummyPatient())
                 .time(ZonedDateTime.now());
 
